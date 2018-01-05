c2f7d8b0b5ef6300e842ebd20a38047880df781b
marknre <hurricanemark@gmail.com>
2018-01-05 15:39:49 -0800

    servicing the landing / page with json response.

--- api.py
+++ api.py
@@ -6,8 +6,9 @@ from collections import OrderedDict
 #################################### WebService Route / ##############################################
 class API:
 	def __init__(self, port, local):
+		# During initialization, launch the _route() method to register the routes enabled
 		self._app = Bottle()
-		self._route() # During initialisation we launch the _route() method to register the routes enabled
+		self._route()
 
 		self._local = local
 		self._port = port
@@ -21,13 +22,19 @@ class API:
 		self._app.run(server='paste', host=self._host, port=self._port)
 
 	def _route(self):
-		self._app.hook('before_request')(self._strip_path) # Needed to prevent errors.
-		self._app.route('/', callback=self._homepage) # We tell to the API to listen on "/" and execute the restapionmark  "_homepage()" when "/" is called
+		# _app.hook is needed to prevent errors.
+		self._app.hook('before_request')(self._strip_path) 
 
-		# We tell the API to execute _doAction() when a POST Request is perform on "/restapionmark"
+		# Tell the API to listen on "/" and execute the restapionmark  "_homepage()" when "/" is called
+		self._app.route('/', callback=self._homepage) 
+
+		# Tell the API to execute _doAction() when a POST Request is perform on "/action"
+                # e.g. http://127.0.0.1:7777/action
 		self._app.route('/action', method="POST", callback=self._doAction)
 
-		# We tell the API to execute _doAction() when a GET Request is perform on "/restapionmark"
+		# Tell the API to execute _doGetMark() when a GET Request is perform on "/restapionmark"
+                # e.g. http://127.0.0.1:7777/restapionmark
+		# note that your external ipaddress should work here too.
 		self._app.route('/restapionmark', method="GET", callback=self._doGetMark)
 
 	def _strip_path(self):
@@ -37,12 +44,16 @@ class API:
 		return static_file("index.html", root=os.getcwd()+'\\html') # We return the "index.html" file as a static file.
 
 	def _doAction(self):
-		rv = {"status": "Success"} # We create a dictionary with the key "status" = "success"
-		response.content_type = 'application/json' # we set the correct "response.content_type" to "json" format, because "json" is nice and cool !
+		# service the landing page with json reponse
+		# Create a dictionary with the key "status" = "success" 
+		rv = {"status": "Success"} 
+		# Set the correct "response.content_type" to "json" format
+		response.content_type = 'application/json' 
 
 		return dumps(rv) # We dump the dictionary into json file and return it.
 	def _doGetMark(self):
- 
+		# for simplicity, a static dictionary.  
+		# in real world practices, it should come from dynamic source such as SQL querry, load file, etc.
 		my_attributes = OrderedDict ({
 			"Name": "Mark V. Nguyen",
 			"Current position": "Principal software engineer",
@@ -63,10 +74,9 @@ class API:
 			}
 		})
 
-		#print(json.dumps(my_dictionary))
+		#print(json.dumps(my_attributes))
 		rv = my_attributes
-		#rv = {"status": "Employed", "Mark": "knows RESTful API", "} # We create a dictionary with the key "status" = "success"
-		#rv = {"status": "Employed", "Mark": "knows RESTful API", "} # We create a dictionary with the key "status" = "success"
-		response.content_type = 'application/json' # we set the correct "response.content_type" to "json" format, because "json" is nice and cool !
-
-		return dumps(rv, indent=4) # We dump the dictionary into json file and return it.
+		# set the correct "response.content_type" to JSON format
+		response.content_type = 'application/json' 
+		# return the dictionary using json.dumps 
+		return dumps(rv, indent=4) 
